e2856ed62e668ff25795e6f8b8dced9a307789fe
Quadri Adedayo Ismail <ismaildeve070@gmail.com>
2020-05-31 14:41:47 -0700

    Updates

---
+++ precache-manifest.4cb04ba44e251b48e8e34a7ac4a02967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b04950ce5ad24703d961a8bb8f1cd8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2615faa4a9b38dd31019",
+    "url": "/my-app/static/css/main.ddc5cbf6.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/my-app/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2615faa4a9b38dd31019",
+    "url": "/my-app/static/js/main.a2a429fe.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
